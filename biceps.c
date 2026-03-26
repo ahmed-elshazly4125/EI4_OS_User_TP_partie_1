ab13ceb66f5d45a6c5f0b9c56e42b50a34cca86e
ahmed-elshazly4125 <aelshazl4125@gmail.com>
2026-03-26 15:08:26 +0100

    Ajoute une boucle interactive avec readline

--- biceps.c
+++ biceps.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <readline/readline.h>
 
 int main(void)
 {
+    char *ligne;
+
+    while (1) {
+        ligne = readline("biceps$ ");
+        if (ligne == NULL) {
+            putchar('\n');
+            break;
+        }
+
+        printf("Commande saisie : [%s]\n", ligne);
+        free(ligne);
+    }
+
     return 0;
 }
