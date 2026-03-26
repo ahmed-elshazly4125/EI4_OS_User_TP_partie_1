e11b18b729a0eb6691b60375f08a1e34eb725e4a
ahmed-elshazly4125 <aelshazl4125@gmail.com>
2026-03-26 16:38:29 +0100

    codes de la partie 1 et la partie 2 avec des capture d'ecran du test des codes

---
+++ partie2.c
@@ -0,0 +1,141 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <limits.h>
+#include <readline/readline.h>
+
+static char **Mots;
+static int NMots;
+
+char *copyString(char *s)
+{
+    char *copie;
+    size_t taille;
+
+    taille = strlen(s) + 1;
+    copie = malloc(taille);
+    if (copie == NULL) {
+        perror("malloc");
+        exit(EXIT_FAILURE);
+    }
+
+    strcpy(copie, s);
+    return copie;
+}
+
+void libereAnalyse(void)
+{
+    int i;
+
+    for (i = 0; i < NMots; i++) {
+        free(Mots[i]);
+    }
+
+    free(Mots);
+    Mots = NULL;
+    NMots = 0;
+}
+
+int analyseCom(char *b)
+{
+    char *travail;
+    char *courant;
+    char *mot;
+    char **nouveau;
+
+    libereAnalyse();
+
+    travail = copyString(b);
+    courant = travail;
+
+    while ((mot = strsep(&courant, " \t\n")) != NULL) {
+        if (*mot == '\0') {
+            continue;
+        }
+
+        nouveau = realloc(Mots, (NMots + 2) * sizeof(char *));
+        if (nouveau == NULL) {
+            perror("realloc");
+            free(travail);
+            exit(EXIT_FAILURE);
+        }
+
+        Mots = nouveau;
+        Mots[NMots] = copyString(mot);
+        NMots++;
+        Mots[NMots] = NULL;
+    }
+
+    free(travail);
+    return NMots;
+}
+
+char *fabrique_prompt(void)
+{
+    char *user;
+    char hostname[HOST_NAME_MAX + 1];
+    char *prompt;
+    char fin_prompt;
+    size_t taille;
+
+    user = getenv("USER");
+    if (user == NULL) {
+        user = "user";
+    }
+
+    if (gethostname(hostname, sizeof(hostname)) != 0) {
+        strcpy(hostname, "machine");
+    }
+    hostname[sizeof(hostname) - 1] = '\0';
+
+    if (geteuid() == 0) {
+        fin_prompt = '#';
+    } else {
+        fin_prompt = '$';
+    }
+
+    taille = strlen(user) + strlen(hostname) + 4;
+    prompt = malloc(taille);
+    if (prompt == NULL) {
+        perror("malloc");
+        exit(EXIT_FAILURE);
+    }
+
+    snprintf(prompt, taille, "%s@%s%c ", user, hostname, fin_prompt);
+    return prompt;
+}
+
+int main(void)
+{
+    char *ligne;
+    char *prompt;
+    int i;
+
+    Mots = NULL;
+    NMots = 0;
+
+    while (1) {
+        prompt = fabrique_prompt();
+        ligne = readline(prompt);
+        free(prompt);
+
+        if (ligne == NULL) {
+            putchar('\n');
+            break;
+        }
+
+        if (analyseCom(ligne)) {
+            printf("Commande : %s\n", Mots[0]);
+
+            for (i = 1; i < NMots; i++) {
+                printf("Parametre %d : %s\n", i, Mots[i]);
+            }
+        }
+
+        free(ligne);
+    }
+
+    libereAnalyse();
+    return 0;
+}
