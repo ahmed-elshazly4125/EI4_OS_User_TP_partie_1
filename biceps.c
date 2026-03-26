ac53c42ee22418f994c29bc4c0cc65c6c8dbe0b6
ahmed-elshazly4125 <aelshazl4125@gmail.com>
2026-03-26 15:11:18 +0100

    Affiche le nom de l'utilisateur dans le prompt

--- biceps.c
+++ biceps.c
@@ -1,13 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <readline/readline.h>
+
+char *fabrique_prompt(void)
+{
+    char *user;
+    char *prompt;
+    size_t taille;
+
+    user = getenv("USER");
+    if (user == NULL) {
+        user = "user";
+    }
+
+    taille = strlen(user) + 3;
+    prompt = malloc(taille);
+    if (prompt == NULL) {
+        perror("malloc");
+        exit(EXIT_FAILURE);
+    }
+
+    snprintf(prompt, taille, "%s$ ", user);
+    return prompt;
+}
 
 int main(void)
 {
     char *ligne;
+    char *prompt;
 
     while (1) {
-        ligne = readline("biceps$ ");
+        prompt = fabrique_prompt();
+        ligne = readline(prompt);
+        free(prompt);
+
         if (ligne == NULL) {
             putchar('\n');
             break;
